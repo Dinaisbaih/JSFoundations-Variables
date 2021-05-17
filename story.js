489a6a8cb96fd44fee641bd35d8551634463c5a2
Dinaisbaih <isbaihdina.f@outlook.com>
2021-05-17 17:49:36 +0300

    Task Done

---
+++ story.js
@@ -0,0 +1,6 @@
+let name = "Dina";
+let age = 26;
+let hoppy = "KickBoxing";
+
+console.log (`Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hoppy} practice!`)
+
